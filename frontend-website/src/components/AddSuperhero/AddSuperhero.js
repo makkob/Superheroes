58f5771bff458454cdcb521e133ed587d25d105d
makkob <maximkabig945@gmail.com>
2022-10-25 14:47:14 +0300

    form

--- frontend-website/src/components/AddSuperhero/AddSuperhero.js
+++ frontend-website/src/components/AddSuperhero/AddSuperhero.js
@@ -2,5 +2,25 @@ import React from "react";
 import styles from "./AddSuperhero.module.css";
 
 export default function AddSuperhero() {
-  return <div>AddSuperhero</div>;
+  return (
+    <form className={styles.modal}>
+      <p>Form for add superhero</p>
+      <label>
+        Nickname: <input type="text" name="nickname" />
+      </label>{" "}
+      <label>
+        Real name: <input type="text" name="real_name" />
+      </label>{" "}
+      <label>
+        Origin description: <input type="text" name="origin_description" />
+      </label>{" "}
+      <label>
+        Catch phrase: <input type="text" name="catch_phrase" />
+      </label>{" "}
+      <label>
+        Image: <input type="file" name="filename" />
+      </label>{" "}
+      <input type="submit" value="Submit" />
+    </form>
+  );
 }
